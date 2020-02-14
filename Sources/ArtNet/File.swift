2e993612d4f172ccd6cb3f97fcd95edcf1512bc6
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-02-13 19:12:13 -0500

    Added Style

---
+++ Sources/ArtNet/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 2/13/20.
+//
+
+import Foundation
---
+++ Sources/ArtNet/Style.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 2/13/20.
+//
+
+/**
+ The following table details the Style codes.
+ The Style code defines the general functionality of a Controller.
+ The Style code is returned in ArtPollReply.
+ */
+public enum Style: UInt8, CaseIterable, Codable {
+    
+    /// A DMX to / from Art-Net device
+    case node           = 0x00
+    
+    /// A lighting console.
+    case controller     = 0x01
+    
+    /// A Media Server.
+    case media          = 0x02
+    
+    /// A network routing device.
+    case route          = 0x03
+    
+    /// A backup device.
+    case backup         = 0x04
+    
+    /// A configuration or diagnostic tool.
+    case configuration  = 0x05
+    
+    /// A visualiser.
+    case visualizer     = 0x06
+}
