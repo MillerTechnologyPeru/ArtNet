f9dde22eff204300cfa6fa41bbd1074be9c4ff92
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-02-21 11:57:24 -0500

    Updated unit tests

--- Tests/ArtNetTests/ArtNetTests.swift
+++ Tests/ArtNetTests/ArtNetTests.swift
@@ -7,13 +7,24 @@ final class ArtNetTests: XCTestCase {
         ("testPoll", testPoll),
     ]
     
+    func testID() {
+        
+        let string = "Art-Net"
+        let id = ArtNetHeader.ID.artNet
+        XCTAssertEqual(id.description, string)
+        XCTAssertEqual(id.rawValue, string)
+        XCTAssertEqual(id, "Art-Net")
+        XCTAssertEqual(id.data, Data([0x41, 0x72, 0x74, 0x2D, 0x4E, 0x65, 0x74, 0x00]))
+        XCTAssertEqual(id, ArtNetHeader.ID(data: id.data))
+    }
+    
     func testPoll() {
         
         let poll = ArtPoll(
             behavior: [.diagnostics],
-            priority: [.low]
+            priority: .low
         )
         
-        
+        XCTAssertEqual(poll.protocolVersion, .current)
     }
 }
