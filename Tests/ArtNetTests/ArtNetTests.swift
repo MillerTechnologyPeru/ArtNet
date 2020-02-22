a74f7f5bffed6eb1bcef6712f6131047f2d8a81e
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-02-21 19:49:12 -0500

    Updated unit test

--- Tests/ArtNetTests/ArtNetTests.swift
+++ Tests/ArtNetTests/ArtNetTests.swift
@@ -32,13 +32,38 @@ final class ArtNetTests: XCTestCase {
             encoder.log = { print("Encoder:", $0) }
             let encodedData = try encoder.encode(value)
             
-            print("[" + encodedData.reduce("", { $0 + ($0.isEmpty ? "" : ", ") + "0x" + String($1, radix: 16).uppercased() }) + "]")
+            print(encodedData.hexString)
             
             XCTAssertFalse(encodedData.isEmpty)
-            XCTAssertEqual(encodedData, Data([0x41, 0x72, 0x74, 0x2D, 0x4E, 0x65, 0x74, 0x00, 0x00, 0x20, 0x0E, 0x00, 0x02, 0x10]))
+            XCTAssertEqual(encodedData, Data([
+                0x41, 0x72, 0x74, 0x2D, 0x4E, 0x65, 0x74, 0x00, // Art-Net
+                0x00, 0x20,                                     // Opcode: 0x2000
+                0x0E, 0x00,                                     // Protocol version: 14
+                0x02,                                           // TalkToMe: Diagnostics (0b01)
+                0x10])                                          // Diagnostic: Low (0x10)
+            )
         } catch {
             XCTFail(error.localizedDescription)
             dump(error)
         }
+    }
+}
+
+extension Sequence where Element == UInt8 {
+    
+    var hexString: String {
+        return "[" + reduce("", { $0 + ($0.isEmpty ? "" : ", ") + "0x" + $1.toHexadecimal().uppercased() }) + "]"
+    }
+}
+
+internal extension UInt8 {
+    
+    func toHexadecimal() -> String {
+        
+        var string = String(self, radix: 16)
+        if string.count == 1 {
+            string = "0" + string
+        }
+        return string
     }
 }
