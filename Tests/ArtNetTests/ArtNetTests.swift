a376a90acc5f6397586665221c6ee6982e4fc6f8
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-02-21 19:37:14 -0500

    Updated unit tests

--- Tests/ArtNetTests/ArtNetTests.swift
+++ Tests/ArtNetTests/ArtNetTests.swift
@@ -20,11 +20,25 @@ final class ArtNetTests: XCTestCase {
     
     func testPoll() {
         
-        let poll = ArtPoll(
-            behavior: [.diagnostics],
-            priority: .low
-        )
-        
-        XCTAssertEqual(poll.protocolVersion, .current)
+        do {
+            let value = ArtPoll(
+                behavior: [.diagnostics],
+                priority: .low
+            )
+            
+            XCTAssertEqual(value.protocolVersion, .current)
+            
+            var encoder = ArtNetEncoder()
+            encoder.log = { print("Encoder:", $0) }
+            let encodedData = try encoder.encode(value)
+            
+            print("[" + encodedData.reduce("", { $0 + ($0.isEmpty ? "" : ", ") + "0x" + String($1, radix: 16).uppercased() }) + "]")
+            
+            XCTAssertFalse(encodedData.isEmpty)
+            XCTAssertEqual(encodedData, Data([0x41, 0x72, 0x74, 0x2D, 0x4E, 0x65, 0x74, 0x00, 0x00, 0x20, 0x0E, 0x00, 0x02, 0x10]))
+        } catch {
+            XCTFail(error.localizedDescription)
+            dump(error)
+        }
     }
 }
